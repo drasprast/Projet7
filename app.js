c254d83b4e6cc361c76acf04bdde6b3ecc20a54d
Thomas Bilbault <thomas.bilbault@gmail.com>
2023-08-31 22:35:47 +0200

    ajout schema

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use("/api/books", (req, res, next) => {
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://drasprast:8scaOED7zQV9kwNu@cluster0.e6sc4d5.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/api/books", (req, res, next) => {
   const book = [
     {
       userId:
