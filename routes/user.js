0b455a97d56223aa662f53a92fbf744208ab877d
Thomas Bilbault <thomas.bilbault@gmail.com>
2023-09-05 14:26:05 +0200

    rename

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user");
-const limiter = require("../middlewares/ratelimiter");
-const passwordValidator = require("../middlewares/passwordValidator");
+const limiter = require("../middlewares/rateLimit");
+const passwordValidation = require("../middlewares/passwordValidation");
 
-router.post("/signup", passwordValidator, limiter, userController.signup);
+router.post("/signup", passwordValidation, limiter, userController.signup);
 router.post("/login", limiter, userController.login);
 
 module.exports = router;
